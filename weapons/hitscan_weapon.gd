524f2d3a0e4bdfa2a16474c2f11e4bd182d50643
BlurryGames <julian.lb.games@gmail.com>
2024-06-02 19:22:37 +0200

    Section4: Robo Rampage / Lesson13: Recoil and Raycasts

--- weapons/hitscan_weapon.gd
+++ weapons/hitscan_weapon.gd
@@ -1,11 +1,23 @@
 class_name HitscanWeapon extends Node3D
 
-@export var fire_rate: float = 14.0
+@export var weapon_mesh: Node3D = null
 
+@export var fire_rate: float = 14.0
+@export var recoil: float = 0.05
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
 @onready var cooldown_timer: Timer = $CooldownTimer
+
+@onready var weapon_position: Vector3 = weapon_mesh.position
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
 		if cooldown_timer.is_stopped():
-			cooldown_timer.start(1.0 / fire_rate)
-			print("Weapon fired!")
+			shoot()
+	
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, 10.0 * delta)
+
+func shoot() -> void:
+	cooldown_timer.start(1.0 / fire_rate)
+	printt("Weapon fired!", str(ray_cast_3d.get_collider()))
+	weapon_mesh.position.z += recoil
