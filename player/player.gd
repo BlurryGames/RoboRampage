e630179f9787921d28fb73b6fa8a58e42aab550d
BlurryGames <julian.lb.games@gmail.com>
2024-06-02 11:30:47 +0200

    Section4: Robo Rampage / Lesson7: Advanced Jumping

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,9 @@
 class_name Player extends CharacterBody3D
 
 const SPEED: float = 5.0
-const JUMP_VELOCITY: float = 4.5
+
+@export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.5
 
 var mouse_motion := Vector2.ZERO
 
@@ -15,10 +17,13 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		if velocity.y >= 0:
+			velocity.y -= gravity * delta
+		else:
+			velocity.y -= gravity * fall_multiplier * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
 
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
